200af60adb6cf94f3283a8374efc9970956203ff
EugeneSeliv <65186568+EugeneSeliv@users.noreply.github.com>
2020-08-25 12:25:05 +0300

    HomeWork-4, this, apply, call, bind

--- main.js
+++ main.js
@@ -1,95 +1,91 @@
-$btnDamage = document.getElementById('btn-damage');
-$btnRecovery = document.getElementById('btn-recovery');
+(function () {
+  $btnDamage = document.getElementById('btn-damage');
+  $btnRecovery = document.getElementById('btn-recovery');
 
-const character = {
-  name: 'Pikachu',
-  defaultHP: 100,
-  demageHP: 100,
-  strikeCount: 0,
-  healCount: 0,
-  elHP: document.getElementById('health-character'),
-  elProgressbar: document.getElementById('progressbar-character'),
-  avatar: document.getElementById('pikachu'),
-}
+  const character = {
+    name: 'Pikachu',
+    defaultHP: 200,
+    demageHP: 100,
+    strikeCount: 0,
+    healCount: 0,
+    addStrikeCount,
+    addHealCount,
+    changeHP,
+    elHP: document.getElementById('health-character'),
+    elProgressbar: document.getElementById('progressbar-character'),
+    avatar: document.getElementById('pikachu'),
+  }
 
-const enemy = {
-  name: 'Charmander',
-  defaultHP: 100,
-  demageHP: 100,
-  strikeCount: 0,
-  healCount: 0,
-  elHP: document.getElementById('health-enemy'),
-  elProgressbar: document.getElementById('progressbar-enemy'),
-  avatar: document.getElementById('charmander'),
-}
+  const enemy = {
+    name: 'Charmander',
+    defaultHP: 100,
+    demageHP: 100,
+    strikeCount: 0,
+    healCount: 0,
+    addStrikeCount,
+    addHealCount,
+    changeHP,
+    elHP: document.getElementById('health-enemy'),
+    elProgressbar: document.getElementById('progressbar-enemy'),
+    avatar: document.getElementById('charmander'),
+  }
 
-function renderHPLife(person) {
-  person.elHP.innerText = person.demageHP + ' / ' + person.defaultHP;
-}
+  function changeHP(count) {
+    this.demageHP -= count;
+    if (this.demageHP < 0) this.demageHP = 0;
+    if (this.demageHP > this.defaultHP) this.demageHP = this.defaultHP;
+    this.elHP.innerText = this.demageHP + ' / ' + this.defaultHP;
+    this.elProgressbar.style.width = 100 * this.demageHP / this.defaultHP + '%';
+    if (this.demageHP <= 0) this.avatar.style.opacity = 0.1;
+  }
 
-function renderProgressburHP(person) {
-  person.elProgressbar.style.width = person.demageHP + '%';
-}
+  function random(num) {
+    return Math.ceil(Math.random() * num);
+  }
 
-function renderHP(person) {
-  renderHPLife(person);
-  renderProgressburHP(person);
-  if (person.demageHP <= 0) person.avatar.style.opacity = 0.2;
-}
+  function addStrikeCount() {
+    this.strikeCount += 1;
+  }
 
-function changeHP(count, person) {
-  person.demageHP -= count;
-  if (person.demageHP < 0) person.demageHP = 0;
-  if (person.demageHP > 100) person.demageHP = 100;
-  renderHP(person);
-}
+  function addHealCount() {
+    this.healCount += 1;
+  }
 
-function random(num) {
-  return Math.ceil(Math.random() * num);
-}
+  function init() {
+    character.changeHP(0);
+    enemy.changeHP(0);
+    character.strikeCount = random(2) - 1;
+    character.healCount = random(2) - 1;
+    enemy.strikeCount = random(2) - 1;
+    enemy.healCount = random(2) - 1;
+    $btnDamage.addEventListener('click', function () {
+      if (character.strikeCount <= enemy.strikeCount) {
+        character.changeHP(random(20));
+        character.addStrikeCount();
+      } else {
+        enemy.changeHP(random(20));
+        enemy.addStrikeCount();
+      }
+      if (character.demageHP <= 0 || enemy.demageHP <= 0) {
+        $btnDamage.disabled = true;
+        $btnRecovery.disabled = true;
+        $btnDamage.style.opacity = 0.2;
+        $btnRecovery.style.opacity = 0.2;
+        setTimeout(() => {
+          alert('GAME OVER');
+        }, 0);
+      }
+    });
+    $btnRecovery.addEventListener('click', function () {
+      if (character.healCount <= enemy.healCount) {
+        character.changeHP(- random(15));
+        character.addHealCount();
+      } else {
+        enemy.changeHP(- random(15));
+        enemy.addHealCount();
+      }
+    });
+  }
 
-function strikeCount(person) {
-  person.strikeCount += 1;
-}
-
-function healCount(person) {
-  person.healCount += 1;
-}
-
-function init() {
-  renderHP(character);
-  renderHP(enemy);
-  character.strikeCount = random(2) - 1;
-  character.healCount = random(2) - 1;
-  enemy.strikeCount = random(2) - 1;
-  enemy.healCount = random(2) - 1;
-  $btnDamage.addEventListener('click', function () {
-    if (character.strikeCount <= enemy.strikeCount) {
-      changeHP(random(20), character);
-      strikeCount(character);
-    } else {
-      changeHP(random(20), enemy);
-      strikeCount(enemy);
-    }
-    if (character.demageHP <= 0 || enemy.demageHP <= 0) {
-      $btnDamage.disabled = true;
-      $btnRecovery.disabled = true;
-      $btnDamage.style.opacity = 0.2;
-      $btnRecovery.style.opacity = 0.2;
-      setTimeout(() => {
-        alert('GAME OVER');
-      }, 0);
-    }
-  });
-  $btnRecovery.addEventListener('click', function () {
-    if (character.healCount <= enemy.healCount) {
-      changeHP(- random(15), character);
-      healCount(character);
-    } else {
-      changeHP(- random(15), enemy);
-      healCount(enemy);
-    }
-  });
-}
-
-init();
+  init();
+})()
