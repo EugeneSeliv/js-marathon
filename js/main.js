b5c676526a453636739f491662c2c76ce92dbeb6
EugeneSeliv <65186568+EugeneSeliv@users.noreply.github.com>
2020-09-03 16:08:03 +0300

    Add pokemons

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-import { CHARACTERS, BUTTONS } from './config.js';
+import POKEMONS from './pokemons.js';
 import { Pokemon } from './characters.js';
 import renderLog from './fight-log.js';
 import {
@@ -9,16 +9,17 @@ import {
   addButtonsClickLimit
 } from './functions.js';
 
+const BUTTONS = [20, 15];
+
 function init() {
   const player1Container = 'character-1';
   const player2Container = 'character-2';
-  const player1 = new Pokemon(CHARACTERS[0]);
-  const player2 = new Pokemon(CHARACTERS[1]);
+  const player1 = new Pokemon(POKEMONS[0]);
+  const player2 = new Pokemon(POKEMONS[1]);
   renderDomElement(player1.initDomData(), player1Container);
   renderDomElement(player2.initDomData(), player2Container);
   player1.initControls(player1Container);
   player2.initControls(player2Container);
-  console.log(player1);
 
   const $btnDamage = $getElById('btn-damage');
   const $btnRecovery = $getElById('btn-recovery');
